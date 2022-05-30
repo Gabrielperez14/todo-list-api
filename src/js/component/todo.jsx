30e863daeb481e6955c6c77f64a2b462777a6f53
Gabriel Perez <gabrielperezpernia@gmail.com>
2022-05-30 12:09:52 -0400

    [CREATE]todo/list/fetch

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,59 +1,34 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
 const URL_BASE = "https://assets.breatheco.de/apis/fake/todos/user";
-
+const initialState = { label: "", done: false };
 const Todos = () => {
-	const [todo, setTodo] = useState("");
-
+	const [todo, setTodo] = useState(initialState);
 	const [todoList, setTodoList] = useState([]);
-
 	const [error, setError] = useState(false);
-
 	const handleChange = (event) => {
-		setTodo(event.target.value);
+		setTodo({ ...todo, [event.target.name]: event.target.value });
 	};
-
 	const handleKeypress = (event) => {
-		if (todo.trim() != "") {
-			if (event.key === "Enter") {
-				setTodoList([...todoList, todo]);
-				// setTodo("");
-				setError(false);
-			}
-		} else {
-			setError(true);
+		if (event.key === "Enter") {
+			putTodo();
+			getTodos();
 		}
-	};
-
-	const handleDelete = (id) => {
-		const newTodoList = todoList.filter((item, index) => id != index);
-		setTodoList(newTodoList);
 	};
 
 	async function getTodos() {
 		try {
-			let response = await fetch(`${URL_BASE}/GabrielPerez`, {
-				mode: "cors",
-				method: "GET",
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-				},
-			});
-
+			let response = await fetch(`${URL_BASE}/GabrielPerez`);
+			let results = await response.json();
 			if (response.ok) {
-				// let results = await response.json();
-				console.log(response);
-				// setTodoList(results);
+				setTodoList(results);
 			} else {
-				console.log("else");
 				newUser();
 			}
 		} catch (error) {
 			console.log(error);
 		}
 	}
-
 	function newUser() {
 		fetch(`${URL_BASE}/GabrielPerez`, {
 			method: "POST",
@@ -70,33 +45,95 @@ const Todos = () => {
 		});
 	}
 
+	const putTodo = async () => {
+		try {
+			if (todo.label.trim() != "") {
+				let response = await fetch(`${URL_BASE}/GabrielPerez`, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify([...todoList, todo]),
+				});
+				if (response.ok) {
+					getTodos();
+					setError(false);
+				} else {
+					console.log(response.status);
+				}
+			} else {
+				setError(true);
+				return;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const deleteTodo = async (id) => {
+		try {
+			let newListTodo = todoList.filter((item, index) => index != id);
+			let response = await fetch(`${URL_BASE}/GabrielPerez`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newListTodo),
+			});
+			if (response.ok) {
+				getTodos();
+			} else {
+				console.log(response.status);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const deleteList = async () => {
+		try {
+			let response = await fetch(`${URL_BASE}/GabrielPerez`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			if (response.ok) {
+				alert("su lista fue eliminada correctamente");
+				getTodos();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	useEffect(() => {
 		getTodos();
 	}, []);
-
 	return (
 		<div className="row justify-content-center flex-column">
 			<div className="container col-4">
 				<ul className="list-group">
 					<li className="list-group-item">
-						<input
-							className="entrada"
-							type="text"
-							placeholder="Wha needs to be done?"
-							value={todo}
-							onChange={handleChange}
-							onKeyPress={handleKeypress}
-						/>
+						{
+							<input
+								className="entrada"
+								type="text"
+								placeholder="Wha needs to be done?"
+								name="label"
+								value={todo.label}
+								onChange={handleChange}
+								onKeyPress={handleKeypress}
+							/>
+						}
 					</li>
 				</ul>
 			</div>
-
 			{error && (
 				<div className="alert alert-danger">
 					El campo no puede estar vacio
 				</div>
 			)}
-
 			<div className="container col-4">
 				{todoList.map((item, index) => {
 					return (
@@ -104,13 +141,19 @@ const Todos = () => {
 							{item.label}
 							<div
 								className="borrar"
-								onClick={() => handleDelete(index)}>
+								onClick={() => deleteTodo(index)}>
 								X
 							</div>
 						</li>
 					);
 				})}
 				<li className="list-group-item">{todoList.length} item left</li>
+				<button
+					type="button"
+					className="btn btn-success mt-2"
+					onClick={() => deleteList()}>
+					borrar lista
+				</button>
 			</div>
 		</div>
 	);
